90953779c25ed6e8603c52dcaaf2d8b753a9b2cc
Shin Yang <shin.yang@linkers.net>
2020-07-02 15:25:59 +0800

    Complete challenge tests

--- 03_method/try_over3_3.rb
+++ 03_method/try_over3_3.rb
@@ -101,16 +101,23 @@ end
 module TryOver3::TaskHelper
   def self.included(klass)
     klass.define_singleton_method :task do |name, &task_block|
-      new_klass = Class.new do
-        define_singleton_method :run do
-          puts "start #{Time.now}"
-          block_return = task_block.call
-          puts "finish #{Time.now}"
-          block_return
-        end
+      define_singleton_method name do
+        puts "start #{Time.now}"
+        block_return = task_block.call
+        puts "finish #{Time.now}"
+        block_return
       end
-      new_klass_name = name.to_s.split("_").map{ |w| w[0] = w[0].upcase; w }.join
-      const_set(new_klass_name, new_klass)
+
+      define_singleton_method :const_missing do |const|
+        super(const) unless klass.respond_to?(const.downcase)
+        
+        obj = Object.new
+        obj.define_singleton_method :run do
+          warn "Warning: #{klass}::#{const}.run is deprecated"
+          klass.send name
+        end
+        obj
+      end
     end
   end
 end
